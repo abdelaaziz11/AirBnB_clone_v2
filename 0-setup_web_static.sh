6bfef577f7897887d2b6dac823643e081904617e
Hamid <Bouayadihamid@gmail.com>
2024-04-04 02:38:33 -0700

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,32 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-
-# Update package index and install Nginx if not already installed
 sudo apt update
-if ! [ -x "$(command -v nginx)" ]; then
-    sudo apt -y install nginx
-fi
+sudo apt -y install nginx
 
-# Create necessary folders if they don't exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-# Create a test HTML file
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 
-# Create symbolic link and update ownership
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration to serve /data/web_static/current/ at /hbnb_static
-if ! grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
-    sudo sed -i '/server_name _;/a \    location /hbnb_static {\n        alias /data/web_static/current;\n    }' /etc/nginx/sites-available/default
-fi
+echo "server {
+    listen 80;
+    server_name _;
+         root /var/www/html;
+    index  index.html index.htm;
 
-# Restart Nginx
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+    location / {
+        # Your other configuration directives, if any
+    }
+}" > /etc/nginx/sites-available/default
+
+# sudo nginx -t
 sudo service nginx restart
-
