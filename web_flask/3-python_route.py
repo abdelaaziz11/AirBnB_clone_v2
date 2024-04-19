7b9789a4bec9d9baa8c57e28bdd869cc8a9ffb92
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-19 11:08:54 -0700

    python lang route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,9 +21,7 @@ def c_text(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    if text is None:
-        return 'Python is cool'
+def python_text(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
