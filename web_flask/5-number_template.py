d22ac396e2c26cfdbbaa48cc5e1dbb0ba5382430
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-19 12:05:04 -0700

    number template route

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""starts a Flask web application, C is fun!"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def intNumber(n):
+    return ("{:d} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
