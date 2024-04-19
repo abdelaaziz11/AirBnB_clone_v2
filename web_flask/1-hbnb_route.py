62db312fa279128740714363962f810805ccd52f
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-19 08:56:53 -0700

    hbnb route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,12 +4,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
 
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
